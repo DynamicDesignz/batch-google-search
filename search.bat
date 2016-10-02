7d822b5bb4342044452a76937ba4de001f08925b
Wazbat <wazbat97@gmail.com>
2016-10-02 12:55:11 +0200

    Update search.bat

--- search.bat
+++ search.bat
@@ -1,6 +1,5 @@
 @echo off
 SETLOCAL
-rem Copyright wazbat you fucks
 color 7c
 echo   ___                _
 echo  / __^|___  ___  __ _^| ^|___
@@ -8,5 +7,5 @@ echo ^| (_ / _ \/ _ \/ _` ^|   -_)
 echo  \___\___/\___/\__, ^|_\___^|
 echo                ^|___/
 echo -------------------------------
-set /p search=Introduza la busqueda: 
+set /p search=Enter your search terms: 
 start "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" "https://www.google.es/search?q=%search%"
